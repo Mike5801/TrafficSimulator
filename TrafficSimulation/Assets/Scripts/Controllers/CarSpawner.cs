e82a34f48604a5dc481993c98fd5e5f0d6cc70ab
AlejandroRuizZ <a01611451@itesm.mx>
2022-11-28 10:45:23 -0600

    UniqueIDs to all Vehicles

--- TrafficSimulation/Assets/Scripts/Controllers/CarSpawner.cs
+++ TrafficSimulation/Assets/Scripts/Controllers/CarSpawner.cs
@@ -7,6 +7,7 @@ public class CarSpawner : MonoBehaviour
     public GameObject rightCar;
     public GameObject leftCar;
     public GameObject CenterCar;
+    public int counter = 10;
     // Start is called before the first frame update
     public void OnEnable(){
         TimeManager.OnMinuteChanged += TimeCheck;
@@ -31,14 +32,25 @@ public class CarSpawner : MonoBehaviour
     {
         int random = Random.Range(0, 3);
         if (random == 0) {
-            Instantiate(rightCar, new Vector3(25, 1.5f, -95), Quaternion.Euler(0, 270, 0));
+            GameObject Car = Instantiate(rightCar, new Vector3(25, 1.5f, 100), Quaternion.Euler(0, 270, 0)) as GameObject;
+            // Assign counter as ID in MoveForward
+            Car.GetComponent<MoveForward>().ID = counter;
+            Debug.Log(Car.GetComponent<MoveForward>().ID);
+            if(Car.GetComponent<MoveForward>().ID == 15){
+                Car.GetComponent<MoveForward>().speed = 0;
+            }
+            counter++;
+
+
+            
+
         }
-        else if (random == 1) {
-            Instantiate(CenterCar, new Vector3(15, 1.5f, -95), Quaternion.Euler(0, 270, 0));
-        }
-        else if (random == 2) {
-            Instantiate(leftCar, new Vector3(5, 1.5f, -95), Quaternion.Euler(0, 270, 0));
-        }
+        // else if (random == 1) {
+        //     Instantiate(CenterCar, new Vector3(15, 1.5f, -95), Quaternion.Euler(0, 270, 0));
+        // }
+        // else if (random == 2) {
+        //     Instantiate(leftCar, new Vector3(5, 1.5f, -95), Quaternion.Euler(0, 270, 0));
+        // }
         yield return null;      
     }
 }
